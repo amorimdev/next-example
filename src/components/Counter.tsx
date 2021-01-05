875f8030b6bfcceac3c5b333c1d77a3cc0769c77
Jhonatan Amorim <jonathanamorim@edirectinsure.com>
2021-01-05 00:35:56 +0000

    fix Counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,18 +1,18 @@
 import {
-  ComponentType,
   useState,
   Children,
   cloneElement,
   Fragment,
+  ReactElement,
 } from 'react';
 
-const Counter = ({ children }: { children: ComponentType[] }) => {
+const Counter = ({ children }: { children: ReactElement[] }) => {
   const [counter, setCounter] = useState(0);
 
   const addCounter = () => setCounter(counter + 1);
   const subCounter = () => setCounter(counter - 1);
 
-  const childrenWithProps = Children.map(children, (element) =>
+  const childrenWithProps = Children.map(children, (element: ReactElement) =>
     cloneElement(element, { addCounter, subCounter })
   );
 
